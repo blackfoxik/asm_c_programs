8671e13143b81a4552ab67f190c003f1d3cd3c08
anton.lisitsyn <anton.lisitsyn.a@gmail.com>
2023-07-26 15:49:14 +0300

    sys calls impl

--- asm_c_greetings/calls.asm
+++ asm_c_greetings/calls.asm
@@ -1,11 +1,32 @@
-%include "stud_io.inc"
-global start
+global sys_read
+global sys_write
+global sys_errno
 
 section .text
-start: mov eax, 0
-again: PRINT "Hello"
-	PUTCHAR 10
-	inc eax
-	cmp eax, 5
-	jl again
-	FINISH
+
+generic_syscall_3:
+    push ebp
+    mov ebp, esp
+    push ebx
+    mov ebx, [ebp+8]
+    mov ecx, [ebp+12]
+    mov edx, [ebp+16]
+    int 80h
+    mov edx, eax
+    and edx, 0fffff000h
+    cmp edx, 0fffff000h
+    jnz .okay
+    mov [sys_errno], eax
+    mov eax, -1
+.okay: pop ebx
+    mov esp, ebp
+    pop ebp
+    ret
+
+sys_read:   mov eax, 3
+            jmp generic_syscall_3
+sys_write:  mov eax, 4
+            jmp generic_syscall_3
+
+section .bss
+sys_errno   resd
