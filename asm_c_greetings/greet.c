aedb472e9c4a190950506d5f3da3c4326e34c02a
anton.lisitsyn <anton.lisitsyn.a@gmail.com>
2023-07-26 16:13:30 +0300

    greet added

--- asm_c_greetings/greet.c
+++ asm_c_greetings/greet.c
@@ -1,5 +1,23 @@
-#include <stdio.h>
-int main() {
-printf("Hello world\n");
-return 0;
+int sys_write(int fd, const void *buf, int size);
+
+static const char dunno[] = "I dont know how to greet you\n";
+static const char hello[] = "Hello, dear ";
+
+static int string_length(const char *s) {
+    int i = 0;
+    while(s[i])
+        i++;
+    return i;
+}
+
+int main(int argc, char **argv) {
+    if(argc < 2) {
+        sys_write(1, dunno, sizeof(dunno)-1);
+        return 1;
+    }
+    sys_write(1, hello, sizeof(hello)-1);
+    sys_write(1, argv[1], string_length(argv[1]));
+    sys_write(1, "\n", 1);
+
+    return 0;
 }
